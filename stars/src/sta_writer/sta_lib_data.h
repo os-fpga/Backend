ba7591891ff8202a72cf4866d72a8a4fa724cabf
taoli4rs <taoli@rapidsilicon.com>
2023-01-11 18:55:13 -0800

    Develope sta file writter to generate general sequential element; enhance stars to handle ff, lut, dsp and bram.

--- stars/src/sta_writer/sta_lib_data.h
+++ stars/src/sta_writer/sta_lib_data.h
@@ -10,26 +10,27 @@ namespace stars {
 typedef enum { INPUT = 0, OUTPUT, INVALID_DIR } port_direction_type;
 typedef enum { POSITIVE = 0, NEGATIVE, INVALID_SENSE } timing_sense_type;
 typedef enum { DATA = 0, CLOCK, RESET, SET, ENABLE, INVALID_PIN_TYPE } pin_type;
+typedef enum { TRANSITION = 0, SETUP, HOLD } timing_type_type;
 
 typedef enum {
   INTERCONNECT = 0,
   LUT,
   BRAM,
   DSP,
-  FLIPFLOP,
+  SEQUENTIAL,
   BLACKBOX,
   INVALID_CELL
 } cell_type;
 
 // cell data container
+class timing_arch;
 class lib_pin {
 private:
   std::string name_;
   int bus_width_;
   port_direction_type dir_;
-  std::vector<lib_pin> related_pins_;
-  timing_sense_type timing_sense_;
   pin_type pin_type_;
+  std::vector<timing_arch> timing_arch_list_;
 
 public:
   lib_pin(){};
@@ -40,15 +41,32 @@ public:
   void bus_width(int value) { bus_width_ = value; }
   port_direction_type direction() const { return dir_; }
   void direction(port_direction_type value) { dir_ = value; }
-  timing_sense_type timing_sense() const { return timing_sense_; }
-  void timing_sense(timing_sense_type value) { timing_sense_ = value; }
   pin_type type() const { return pin_type_; }
   void type(pin_type value) { pin_type_ = value; }
-  void add_related_pin(lib_pin &pin) {
-    related_pins_.push_back(pin);
+  void add_timing_arch(timing_arch &arch) {
+    timing_arch_list_.push_back(arch);
     return;
   }
-  const std::vector<lib_pin> &get_related_pins() const { return related_pins_; }
+  const std::vector<timing_arch> &get_timing_arch() const {
+    return timing_arch_list_;
+  }
+};
+
+class timing_arch {
+private:
+  timing_sense_type sense_;
+  timing_type_type type_;
+  lib_pin related_pin_;
+
+public:
+  timing_arch(){};
+  ~timing_arch(){};
+  timing_sense_type sense() const { return sense_; }
+  void sense(timing_sense_type value) { sense_ = value; }
+  timing_type_type type() const { return type_; }
+  void type(timing_type_type value) { type_ = value; }
+  lib_pin related_pin() const { return related_pin_; }
+  void related_pin(lib_pin value) { related_pin_ = value; }
 };
 
 class lib_cell {
@@ -86,6 +104,23 @@ public:
     }
     return;
   }
+  /*
+  lib_pin &get_pin_by_name(std::string name) {
+    for (auto pin : input_pins_) {
+      if (pin.name() == name) {
+        return pin;
+      }
+    }
+    for (auto pin : output_pins_) {
+      if (pin.name() == name) {
+        return pin;
+      }
+    }
+    null_pin = new lib_pin;
+    null_pin.direction(INVALID_DIR);
+    return null_pin;
+  }
+  */
 };
 
 } // end namespace stars
