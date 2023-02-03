9844b81113d72950583891289c8ab2fb80e5c04b
Alain Dargelas <alain.dargelas@gmail.com>
2023-02-02 17:58:53 -0800

    packer speedup

--- include/packer_fix/output_clustering.h
+++ include/packer_fix/output_clustering.h
@@ -9,7 +9,7 @@ void output_clustering(const vtr::vector<ClusterBlockId, std::vector<t_intra_lb_
 
 void write_packing_results_to_xml(const bool& global_clocks, const std::string& architecture_id, const char* out_fname);
 
-bool check_output_clustering(const vtr::vector<ClusterBlockId, std::vector<t_intra_lb_net>*>& intra_lb_routing, bool global_clocks, const std::unordered_set<AtomNetId>& is_clock, const std::string& architecture_id, const char* out_fname, bool skip_clustering);
+bool check_output_clustering(const vtr::vector<ClusterBlockId, std::vector<t_intra_lb_net>*>& intra_lb_routing, const std::string& architecture_id, const char* out_fname);
 
 void print_stats();
 
