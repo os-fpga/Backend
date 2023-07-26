9158bfdb78954267ae3725fd8c7b50de62727bbc
Sergey Dolgov <sergey.dolgov@rapidsilicon.com>
2023-07-25 21:39:52 -0700

    stars: use $HOME prefix in internal unit tests

--- stars/src/RS/rsOpts.h
+++ stars/src/RS/rsOpts.h
@@ -67,17 +67,14 @@ struct rsOpts {
   bool trace_specified() const noexcept { return traceIndex_ > 0; }
   bool test_id_specified() const noexcept { return test_id_ > 0; }
 
-  bool set_PINC_test() noexcept;
-
   bool set_VPR_TC1() noexcept;  // and2_gemini
   bool set_STA_TC2() noexcept;  // flop2flop, arch 1GE100-ES1
   bool set_STA_TC3() noexcept;  // flop2flop, arch GEMINI
   bool set_STA_TC4() noexcept;  // vex_soc_no_carry
 
-  bool createVprArgv(const vector<string>& W) noexcept;
+  bool createVprArgv(vector<string>& W) noexcept;
 
 private:
-  bool sprintFiles(CStr subdir, CStr stem) noexcept;
   bool set_VPR_TC_args(CStr raw_tc) noexcept;
 };
 
