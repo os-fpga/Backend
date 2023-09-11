01e58411916c1d210d7c23c580d5b044a418080d
Sergey Dolgov <sergey.dolgov@rapidsilicon.com>
2023-09-11 13:11:02 -0700

    pin_c: refactored initRows(), added bitsets for modes

--- pin_c/src/pin_loc/pin.h
+++ pin_c/src/pin_loc/pin.h
@@ -4,6 +4,7 @@
 
 #include "util/geo/xyz.h"
 #include "util/pinc_log.h"
+#include <bitset>
 
 namespace pinc {
 
@@ -12,10 +13,18 @@ using std::vector;
 using StringPair = std::pair<std::string, std::string>;
 
 struct Pin {
+
+  static constexpr uint MAX_PT_COLS = 128;
+
   string udes_pin_name_;
   string device_pin_name_;
   XYZ xyz_;
+
   uint pt_row_   = 0; // row in pin table
+
+  std::bitset<MAX_PT_COLS> rx_modes_;  // indexed by PT-columns, 1 is 'Y'
+  std::bitset<MAX_PT_COLS> tx_modes_;  //
+
   bool is_input_ = false;
   bool is_a2f_   = false;
   bool is_f2s_   = false;
