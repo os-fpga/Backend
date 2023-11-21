432d42bca5dc084b36d2d99f19e2838ea43f896c
Sergey Dolgov <sergey.dolgov@rapidsilicon.com>
2023-11-21 13:53:44 -0800

    stars: clean up nl_partition warnings

--- stars/src/nl_partition/nl_Par.h
+++ stars/src/nl_partition/nl_Par.h
@@ -8,21 +8,27 @@
 
 namespace nlp {
 
+using std::string;
+using std::vector;
+
 class Par {
 public:
   Par(t_pack_molecule* molecule_head);
-  void Bi_Partion(int partion_index);
-  void recursive_partitioning(int num_partitions);
+  ~Par();
+
+  void Bi_Partion(uint partion_index);
+
+  bool recursive_partitioning(int num_partitions);
 
 // DATA:
-  std::vector<t_pack_molecule*> molecules;
-  int numberOfMolecules = 0;
-  int numberOfNets = 0;
-  int numberOfAtoms = 0;
-  int* atomBlockIdToMoleculeId = nullptr;
-  std::string* moleculeIdToName = nullptr;
-  int* partition_array = nullptr;
-  std::vector<int> partitions;
+  vector<t_pack_molecule*> molecules_;
+  uint numMolecules_ = 0;
+  uint numNets_ = 0;
+  uint numAtoms_ = 0;
+  int* atomBlockIdToMolId_ = nullptr;
+  string* molIdToName_ = nullptr;
+  uint* partition_array_ = nullptr;
+  vector<uint> partitions_;
 };
 
 }
