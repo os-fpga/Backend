394f98547a3d377b47128eb5c2927fa9e76ebca0
NadeemYaseen <nadeem.yaseen@rapidsilicon.com>
2024-05-10 19:19:47 +0500

    removed the FlexLM part

--- include/CMAKE_fix/VPR_CMake_RS1.cmake
+++ include/CMAKE_fix/VPR_CMake_RS1.cmake
@@ -2,28 +2,13 @@
 #  1st Rapid Silicon addition to VPR CMake
 #
 
-if (PRODUCTION_BUILD)
-    if(NOT RAPTOR)
-        get_filename_component(FLEX_LM_SRC_DIR "../../../Raptor_Tools/Flex_LM"
-        REALPATH BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
-        add_subdirectory(${FLEX_LM_SRC_DIR} flex_lm)
-    endif()
-    message("Production Build type set to ON")
-    set (PRODUCTION_BUILD_FLAG "-DPRODUCTION_BUILD=1")
-    add_definitions(-DPRODUCTION_BUILD)
-    message("FLEX: "  ${FLEX_LM_SRC_DIR})
-endif(PRODUCTION_BUILD)
-
 option(ENABLE_VERIFIC "Enable Verific front end" OFF)
-
 if (NOT RAPTOR)
-
     if(ENABLE_VERIFIC)
         get_filename_component(VERIFIC_HOME "../../../Raptor_Tools/verific_rs/"
         REALPATH BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}" CACHE)
         add_subdirectory(${VERIFIC_HOME} verific_rs)
     endif()
-
     get_filename_component(READ_EDIF_SRC_DIR "../../../Raptor_Tools/gatelevel_readers/read_edif"
     REALPATH BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}" CACHE)
     add_subdirectory(${READ_EDIF_SRC_DIR} read_edif)
